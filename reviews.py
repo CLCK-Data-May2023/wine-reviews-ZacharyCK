e0c974672e1f0fbb64b40b6a6b7a73068f97259a
Zachary Kohs <zachary.c.kohs@gmail.com>
2023-09-07 12:19:57 -0400

    created solution to problem

--- reviews.py
+++ reviews.py
@@ -1,2 +1,21 @@
 # add your code here
 
+# Add imports to script
+
+import pandas as pd
+import zipfile
+
+
+# open zipped file
+
+# with zipfile.ZipFile('data/winemag-data-130k-v2.csv.zip', 'r') as zip_ref:
+#     zip_ref.extract('winemag-data-130k-v2.csv', 'data')
+
+df = pd.read_csv('data/winemag-data-130k-v2.csv', index_col=0)
+
+solution_data =  (df.groupby(['country']).points.agg(['count', 'mean'])).rename(columns={'mean': 'points'})
+
+
+print(solution_data)
+
+
