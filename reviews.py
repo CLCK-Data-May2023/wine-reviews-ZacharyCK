f4a20c2f1fe786cbb040a5744432398b055efff0
Zachary Kohs <zachary.c.kohs@gmail.com>
2023-09-07 12:30:06 -0400

    updated solution again

--- reviews.py
+++ reviews.py
@@ -15,6 +15,7 @@ df = pd.read_csv('data/winemag-data-130k-v2.csv', index_col=0)
 
 solution_data =  (df.groupby(['country']).points.agg(['count', 'mean'])).rename(columns={'mean': 'points'})
 
+solution_data.to_csv('data/reviews-per-country.csv')
 
 print(solution_data)
 
